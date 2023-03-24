8b6dac9d4357bcc03f872866c26c967903cc5da5
SofyaLogunova <81619039+SofyaLogunova@users.noreply.github.com>
2023-03-24 23:08:45 +0300

    Add files via upload

---
+++ 2.1-2.10/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace _2._1_2._10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Vstavka = new System.Windows.Forms.Button();
+            this.IZvl = new System.Windows.Forms.Button();
+            this.Dlstring = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Vxod = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Vstavka
+            // 
+            this.Vstavka.Location = new System.Drawing.Point(24, 12);
+            this.Vstavka.Name = "Vstavka";
+            this.Vstavka.Size = new System.Drawing.Size(138, 41);
+            this.Vstavka.TabIndex = 0;
+            this.Vstavka.Text = "Вставка";
+            this.Vstavka.UseVisualStyleBackColor = true;
+            this.Vstavka.Click += new System.EventHandler(this.Vstavka_Click);
+            // 
+            // IZvl
+            // 
+            this.IZvl.Location = new System.Drawing.Point(24, 112);
+            this.IZvl.Name = "IZvl";
+            this.IZvl.Size = new System.Drawing.Size(138, 64);
+            this.IZvl.TabIndex = 1;
+            this.IZvl.Text = "Извлечение подстроки";
+            this.IZvl.UseVisualStyleBackColor = true;
+            this.IZvl.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Dlstring
+            // 
+            this.Dlstring.Location = new System.Drawing.Point(24, 59);
+            this.Dlstring.Name = "Dlstring";
+            this.Dlstring.Size = new System.Drawing.Size(138, 43);
+            this.Dlstring.TabIndex = 2;
+            this.Dlstring.Text = "Удаление подстроки";
+            this.Dlstring.UseVisualStyleBackColor = true;
+            this.Dlstring.Click += new System.EventHandler(this.Dlstring_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(24, 257);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 70);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Преобразование строки в число";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Vxod
+            // 
+            this.Vxod.Location = new System.Drawing.Point(24, 182);
+            this.Vxod.Name = "Vxod";
+            this.Vxod.Size = new System.Drawing.Size(138, 57);
+            this.Vxod.TabIndex = 4;
+            this.Vxod.Text = "Вхождение строки";
+            this.Vxod.UseVisualStyleBackColor = true;
+            this.Vxod.Click += new System.EventHandler(this.Vxod_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(24, 410);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(138, 78);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Символ торговой марки";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(24, 347);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(138, 42);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Спец символ";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(327, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(222, 90);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(327, 129);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(222, 90);
+            this.textBox2.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(623, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 42);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Повтор символов";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(623, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 56);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Стоимость машины";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(623, 168);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 51);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Изменение цвета";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 534);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.Vxod);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.Dlstring);
+            this.Controls.Add(this.IZvl);
+            this.Controls.Add(this.Vstavka);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Vstavka;
+        private System.Windows.Forms.Button IZvl;
+        private System.Windows.Forms.Button Dlstring;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button Vxod;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ 2.1-2.10/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2._1_2._10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Vstavka_Click(object sender, EventArgs e)
+        {
+            string partBookTitle = textBox1.Text;
+            string insertText = textBox2.Text;
+            string bookTitle = partBookTitle.Insert(3, insertText);
+            MessageBox.Show(bookTitle);
+        }
+
+        private void Dlstring_Click(object sender, EventArgs e)
+        {
+            string bookTitle = textBox1.Text;
+            bookTitle = bookTitle.Remove(2);
+            MessageBox.Show(bookTitle);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string bookTitle = textBox1.Text;
+            bookTitle = bookTitle.Substring(5, 3);
+            MessageBox.Show(bookTitle);
+        }
+
+        private void Vxod_Click(object sender, EventArgs e)
+        {
+            string str1 = textBox1.Text; 
+            string str2 = textBox2.Text; ;
+            int i = str2.IndexOf(str1);
+            if (i >= 0) MessageBox.Show(str1 + " входит в строку " + str2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string tankman = textBox1.Text;
+            string dog = textBox2.Text;
+            textBox1.Text = tankman + dog; 
+            int all = int.Parse(tankman) + int.Parse(dog);
+            textBox1.Text += Environment.NewLine + all.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int charCode = 169;
+            char specialChar = Convert.ToChar(charCode);
+            textBox1.Text = specialChar.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int charCode = 0x00AE;
+            char specialChar = Convert.ToChar(charCode);
+            textBox1.Text += specialChar.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.String FiveStars = string.Join("", Enumerable.Repeat(textBox1.Text, 5));
+            textBox2.Text = FiveStars;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string AutoName;
+            AutoName = "BMW";
+
+            textBox2.Text =
+            String.Format("Стоимость {0} равна {1:0.0;c}", AutoName, textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            //Color clr = Color.Blue;
+            Color clr = (Color)TypeDescriptor.GetConverter(
+            typeof(Color)).ConvertFromString(textBox1.Text);
+
+            textBox1.Text =
+            (TypeDescriptor.GetConverter(clr).ConvertToString(clr));
+          
+            
+            this.BackColor = clr;
+        }
+    }
+}
