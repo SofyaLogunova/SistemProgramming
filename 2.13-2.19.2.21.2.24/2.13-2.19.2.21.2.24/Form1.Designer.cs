d2f7a185464545aa709380b5d88293d7af3e4a69
SofyaLogunova <81619039+SofyaLogunova@users.noreply.github.com>
2023-04-05 15:51:37 +0300

    Add files via upload

---
+++ 2.13-2.19.2.21.2.24/2.13-2.19.2.21.2.24/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace _2._13_2._19._2._21._2._24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 64);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 46);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(189, 46);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Text = " Переворачиваем строку при помощи C#";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(242, 12);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(189, 46);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(242, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 60);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Замена длинного пути многоточием";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(501, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(189, 60);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Эффект печатающегося текста";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(501, 12);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(189, 46);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 280);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(184, 59);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Соединение строк при помощи StringBuilder";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 209);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(189, 46);
+            this.textBox5.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 364);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(184, 59);
+            this.button5.TabIndex = 9;
+            this.button5.Text = " Получение текущей даты";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(875, 91);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(448, 277);
+            this.listBox1.TabIndex = 10;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(875, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(184, 59);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Вывод даты и времени в разных форматах";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(242, 209);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(189, 59);
+            this.button7.TabIndex = 12;
+            this.button7.Text = " Прибавляем семь дней к текущей дате";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(242, 284);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(189, 55);
+            this.button8.TabIndex = 13;
+            this.button8.Text = " Проверка на високосный год";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(501, 368);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(189, 55);
+            this.button9.TabIndex = 14;
+            this.button9.Text = " Разница дат";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(501, 229);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(189, 46);
+            this.textBox6.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(501, 284);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(189, 46);
+            this.textBox7.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1349, 459);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ 2.13-2.19.2.21.2.24/2.13-2.19.2.21.2.24/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2._13_2._19._2._21._2._24
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("shlwapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool PathCompactPathEx(
+        System.Text.StringBuilder pszOut,
+        string pszSrc,
+        Int32 cchMax,
+        Int32 dwFlags);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //2.13
+        public static string ReverseString(string str)
+        {
+            
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+            
+            
+            StringBuilder revStr = new StringBuilder(str.Length);
+            
+            for (int count = str.Length - 1; count > -1; count--)
+            {
+                revStr.Append(str[count]);
+            }
+            
+            return revStr.ToString();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = ReverseString(textBox2.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //2.14
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+                string strPathFile =
+                "c:/program files/My SuperProgram/skins/sample.txt";
+                StringBuilder sb = new StringBuilder(260);
+                
+                bool b = PathCompactPathEx(sb, strPathFile, 20 + 1, 0);
+                
+                textBox3.Text = sb.ToString();
+            
+        }
+
+        //2.15
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == "Старт")
+            {
+                timer1.Enabled = true;
+                button3.Text = "Стоп";
+            }
+            else
+            {
+                timer1.Enabled = false;
+                
+                button3.Text = "Старт";
+            }
+        }
+        public static int counter = 0;
+      
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            string typingText = "C#.Народные советы";
+            textBox4.Text = typingText.Substring(0, counter);
+            Console.WriteLine($"pract: {typingText.Substring(0, counter)}");
+            counter++;
+            if (counter > typingText.Length)
+                counter = 0;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //2.16
+
+        private string scrollText = "C#.Народные советы ";
+        private void butScroll_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            scrollText = scrollText.Substring(1, scrollText.Length - 1) + scrollText.Substring(0, 1);
+            textBox1.Text = scrollText;
+        }
+        private void butNoScroll_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+        }
+
+        //2.17
+       
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(textBox1.Text);
+            stringBuilder.Append(textBox2.Text);
+            textBox5.Text = stringBuilder.ToString();
+        }
+
+        //2.18
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DateTime today = DateTime.Now;
+            MessageBox.Show(today.Date.ToLongDateString());
+        }
+        //2.19
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+
+            listBox1.Items.Clear();
+            listBox1.Items.Add("d: " + dt.ToString("d"));
+            listBox1.Items.Add("D: " + dt.ToString("D"));
+            listBox1.Items.Add("f: " + dt.ToString("f"));
+            listBox1.Items.Add("F: " + dt.ToString("F"));
+            listBox1.Items.Add("g: " + dt.ToString("g"));
+            listBox1.Items.Add("G: " + dt.ToString("G"));
+            listBox1.Items.Add("m: " + dt.ToString("m"));
+            listBox1.Items.Add("r: " + dt.ToString("r"));
+            listBox1.Items.Add("s: " + dt.ToString("s"));
+            listBox1.Items.Add("u: " + dt.ToString("u"));
+            listBox1.Items.Add("U: " + dt.ToString("U"));
+            listBox1.Items.Add("y: " + dt.ToString("y"));
+            listBox1.Items.Add("MMMM dd: " + dt.ToString("MMMM dd"));
+            listBox1.Items.Add("MM/dd/yyyy: " + dt.ToString("MM/dd/yyyy"));
+            listBox1.Items.Add("MM/dd/yyyy HH:mm: " +
+            dt.ToString("MM/dd/yyyy HH:mm"));
+            listBox1.Items.Add("MM/dd/yyyy hh:mm tt: " +
+            dt.ToString("MM/dd/yyyy hh:mm tt"));
+            listBox1.Items.Add("MM/dd/yyyy H:mm: " +
+            dt.ToString("MM/dd/yyyy H:mm"));
+            listBox1.Items.Add("MM/dd/yyyy h:mm tt: " +
+            dt.ToString("MM/dd/yyyy h:mm tt"));
+            listBox1.Items.Add("MM/dd/yyyy HH:mm:ss: " +
+            dt.ToString("MM/dd/yyyy HH:mm:ss"));
+
+            listBox1.Items.Add("dddd, dd MMMM yyyy: " +
+            dt.ToString("dddd, dd MMMM yyyy"));
+            listBox1.Items.Add("dddd, dd MMMM yyyy HH:mm:ss: " +
+            dt.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+            listBox1.Items.Add("ddd, dd MMM yyyy HH':'mm':'ss 'GMT': " +
+            dt.ToString("ddd, dd MMM yyyy HH':'mm':'ss 'GMT'"));
+
+            listBox1.Items.Add("yyyy'-'MM'-'dd'T'HH':'mm':'ss.fffffffK: " +
+            dt.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss.fffffffK"));
+            listBox1.Items.Add("yyyy'-'MM'-'dd'T'HH':'mm':'ss: " +
+            dt.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss"));
+            listBox1.Items.Add("yyyy MMMM: " + dt.ToString("yyyy MMMM"));
+
+            listBox1.Items.Add("H:mm: " + dt.ToString("H:mm"));
+            listBox1.Items.Add("HH:mm: " + dt.ToString("HH:mm"));
+            listBox1.Items.Add("HH:mm:ss: " + dt.ToString("HH:mm:ss"));
+            listBox1.Items.Add("h:mm tt: " + dt.ToString("h:mm tt"));
+            listBox1.Items.Add("hh:mm tt: " + dt.ToString("hh:mm tt"));
+        }
+
+
+        //2.21
+        private void button7_Click(object sender, EventArgs e)
+        {
+            DateTime curdate = DateTime.Now;
+            DateTime mydate = curdate.AddDays(7);
+            MessageBox.Show(mydate.ToShortDateString());
+        }
+        //2.24
+        private void button8_Click(object sender, EventArgs e)
+        {
+            bool leapYear = DateTime.IsLeapYear(DateTime.Now.Year);
+            MessageBox.Show(
+            String.Format("{0} является високосным годом: {1}",
+            DateTime.Now.Year, leapYear));
+        }
+
+        //Разница дат
+        private void button9_Click(object sender, EventArgs e)
+        {
+            DateTime dt1, dt2;
+            TimeSpan delta; // TimeSpan представляет разницу между двумя датами
+            dt1 = DateTime.Parse(textBox6.Text);
+            dt2 = DateTime.Parse(textBox7.Text);
+            delta = dt2 - dt1;
+            
+
+            MessageBox.Show(Convert.ToString(delta));
+
+
+        }
+    }
+    }
+
