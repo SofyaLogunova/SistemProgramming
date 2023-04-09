383e17c842e4c7471e9a4592aa12b352d7e309bb
SofyaLogunova <81619039+SofyaLogunova@users.noreply.github.com>
2023-04-09 13:12:25 +0300

    Add files via upload

---
+++ 3.11/3.11/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3._11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Random m_Rnd = new Random();
+        private Color RandomRGBColor()
+        {
+            return Color.FromArgb(255, m_Rnd.Next(0, 255),
+            m_Rnd.Next(0, 255), m_Rnd.Next(0, 255));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = RandomRGBColor();
+        }
+    }
+}
