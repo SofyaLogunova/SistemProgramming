a9471f091cbe7a2a69ced7d03c2586fc47be027e
SofyaLogunova <81619039+SofyaLogunova@users.noreply.github.com>
2023-05-21 15:27:27 +0300

    Add files via upload

---
+++ 10.1-10.10/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace _10._1_10._10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "10.1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(150, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(321, 212);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "10.1(второй способ)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 43);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "10.2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 232);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 45);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "10.3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 303);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(104, 43);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "10.4";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(123, 303);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(104, 43);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "10.5";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(503, 26);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(104, 43);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "10.6";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(503, 96);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(104, 43);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "10.7";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(503, 162);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(104, 43);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "10.8";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(503, 298);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(104, 43);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "10.10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(503, 233);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(104, 43);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "10.9";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 368);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ 10.1-10.10/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _10._1_10._10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] astrLogicalDrives = System.IO.Directory.GetLogicalDrives();
+            foreach (string disk in astrLogicalDrives)
+                listBox1.Items.Add(disk);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] astrLogicalDrives = System.Environment.GetLogicalDrives();
+            foreach (string disk in astrLogicalDrives)
+                listBox1.Items.Add(disk);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Выводим информацию о диске
+            System.IO.DriveInfo drv = new System.IO.DriveInfo(@"d:\");
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Диск: " + drv.Name);
+
+            if (drv.IsReady)
+            {
+                listBox1.Items.Add("Тип диска: " + drv.DriveType.ToString());
+                listBox1.Items.Add("Файловая система: " +
+                drv.DriveFormat.ToString());
+                listBox1.Items.Add("Свободное место на диске: " +
+                drv.AvailableFreeSpace.ToString());
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Получим список папок на диске D:
+            listBox1.Items.Clear();
+            string[] astrFolders = System.IO.Directory.GetDirectories(@"d:\");
+            foreach (string folder in astrFolders)
+                listBox1.Items.Add(folder);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Получим список папок, где встречается буквосочетание pro
+            listBox1.Items.Clear();
+            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(@"d:\");
+            System.IO.DirectoryInfo[] folders = di.GetDirectories("*pro*");
+            foreach (System.IO.DirectoryInfo maskdirs in folders)
+                listBox1.Items.Add(maskdirs);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (System.IO.Directory.Exists(@"C:\windows"))
+                label1.Text = "Папка " + @"C:\Windows" + " существует";
+            else
+                label1.Text = "Папка не существует";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string oldPathString = @"C:\MyFolder";
+            string newPathString = @"C:\NewFolder";
+            Directory.Move(oldPathString, newPathString);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Попытаемся удалить папку C:\WUTEMP
+                System.IO.Directory.Delete(@"c:\wutemp");
+                MessageBox.Show("Папка удалена.");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Нельзя удалить непустую папку.");
+            }
+            finally { }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            // задаем папку верхнего уровня
+            fbd.RootFolder = Environment.SpecialFolder.MyComputer;
+            // Заголовок в диалоговом окне
+            fbd.Description = "Выберите папку";
+            // Не выводим кнопку Новая папка
+            fbd.ShowNewFolderButton = false;
+            // Получаем папку, выбранную пользователем
+            if (fbd.ShowDialog() == DialogResult.OK)
+                this.Text = fbd.SelectedPath;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            // Получим путь к папке Мои документы
+            MessageBox.Show(Environment.GetFolderPath(
+             Environment.SpecialFolder.Personal));
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            // Получим информацию о свойствах папки
+            
+            System.IO.DirectoryInfo dir = new
+            System.IO.DirectoryInfo(@"c:\wutemp");
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Проверка папки: " + dir.Name);
+            listBox1.Items.Add("Родительская папка: " + dir.Parent.Name);
+            listBox1.Items.Add("Папка существует: ");
+            listBox1.Items.Add(dir.Exists.ToString());
+            if (dir.Exists)
+            {
+                listBox1.Items.Add("Папка создана: ");
+                listBox1.Items.Add(dir.CreationTime.ToString());
+                listBox1.Items.Add("Папка изменена: ");
+                listBox1.Items.Add(dir.LastWriteTime.ToString());
+                listBox1.Items.Add("Время последнего доступа: ");
+                listBox1.Items.Add(dir.LastAccessTime.ToString());
+                listBox1.Items.Add("Атрибуты папки: ");
+                listBox1.Items.Add(dir.Attributes.ToString());
+                listBox1.Items.Add("Папка содержит: " +
+                dir.GetFiles().Length.ToString() + " файла");
+            }
+        }
+    }
+}
